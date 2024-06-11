9dca5d915b2772788c6357d98e45df9c49688e39
Digvijay25182316 <digvijays.edake2002@gmail.com>
2024-06-11 18:07:38 +0530

    commit

--- ConfigFetch.ts
+++ ConfigFetch.ts
@@ -1,3 +1,3 @@
 export const SERVER_ENDPOINT =
-  // "http://sanjivani-cdd.ap-south-1.elasticbeanstalk.com/sanjivani";
-  "http://localhost:5000/sanjivani";
+  "http://sanjivani-cdd.ap-south-1.elasticbeanstalk.com/sanjivani";
+// "http://localhost:5000/sanjivani";
